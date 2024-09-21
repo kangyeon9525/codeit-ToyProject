4de073cb89a7e7953bb427ca66f930cd3927e229
kangyeon9525 <kangyeon9525@gmail.com>
2024-09-21 11:57:58 +0900

    refactor - 라우트 간결화 & 미들웨어이름 수정

--- src/routes/groupRoutes.js
+++ src/routes/groupRoutes.js
@@ -2,24 +2,20 @@ import express from 'express';
 import { registerGroup, getGroups, updateGroup, deleteGroup, getGroupDetail, verifyGroupPassword, likeGroup, checkGroupIsPublic } from '../controllers/groupController.js';
 import { errorHandler } from '../middlewares/errorHandler.js';
 import { validateGroupRequest, validateUpdateGroupRequest, validateDeleteGroupRequest, 
-  validateGroupDetailRequest, validateVerifyPasswordRequest} from '../middlewares/validation.js';
+  validateGroupDetailRequest, validateVerifyPasswordRequest} from '../middlewares/groupValidation.js';
 
 const router = express.Router();
 
-// 그룹 등록 라우트
-router.post('/groups', validateGroupRequest, registerGroup);
+// 그룹 등록 및 목록 조회 라우트
+router.route('/groups')
+  .post(validateGroupRequest, registerGroup) // 그룹 등록 라우트
+  .get(getGroups); // 그룹 목록 조회
 
-// 그룹 목록 조회 라우트
-router.get('/groups', getGroups);
-
-// 그룹 수정 라우트
-router.put('/groups/:groupId', validateUpdateGroupRequest, updateGroup);
-
-// 그룹 삭제 라우트
-router.delete('/groups/:groupId', validateDeleteGroupRequest, deleteGroup);
-
-// 그룹 상세 정보 조회 라우트
-router.get('/groups/:groupId', validateGroupDetailRequest, getGroupDetail);
+// 그룹 수정, 삭제, 상세 정보 조회 라우트
+router.route('/groups/:groupId')
+  .get(validateGroupDetailRequest, getGroupDetail)  // 그룹 상세 조회
+  .put(validateUpdateGroupRequest, updateGroup)  // 그룹 수정
+  .delete(validateDeleteGroupRequest, deleteGroup);  // 그룹 삭제
 
 // 그룹 조회 권한 확인 라우트
 router.post('/groups/:groupId/verify-password', validateVerifyPasswordRequest, verifyGroupPassword);
