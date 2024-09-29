03aec029fc6c069badf3474dada3d85f3f0d9465
kangyeon9525 <kangyeon9525@gmail.com>
2024-09-29 12:53:12 +0900

    refactor - 뱃지 서비스 에러 처리 수정 & README 업데이트

--- src/routes/imageRoutes.js
+++ src/routes/imageRoutes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { errorHandler } from '../middlewares/errorHandler.js';
 import { uploadImage } from '../controllers/imageController.js';
 import { upload } from '../middlewares/upload.js';
 
@@ -6,5 +7,8 @@ const router = express.Router();
 
 // 이미지 URL 생성 라우트
 router.post('/image', upload, uploadImage);
+
+// 에러 핸들러 미들웨어
+router.use(errorHandler);
 
 export default router;
