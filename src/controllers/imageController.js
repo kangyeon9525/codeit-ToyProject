158f173048bce4bffe1e0f48a4db7d832e3e44bb
kangyeon9525 <kangyeon9525@gmail.com>
2024-10-02 21:51:10 +0900

    feat - aws s3 추가

--- src/controllers/imageController.js
+++ src/controllers/imageController.js
@@ -6,9 +6,11 @@ export const uploadImage = async (req, res, next) => {
 
     // 이미지 업로드된 경우, 이미지 URL 생성
     let imageUrl = '';
-    if (req.files && req.files.image) { // image 필드 확인
-      imageUrl = `${req.protocol}://${req.get('host')}/uploads/${req.files.image[0].filename}`;
-    } else if (req.body.image === '') { // 이미지 URL을 빈 문자열로 보내면 이미지 삭제
+
+    // S3에 업로드된 이미지 URL 가져오기
+    if (req.files && req.files.image) {
+      imageUrl = req.files.image[0].location; // S3 URL
+    } else if (req.body.image === '') {
       return res.status(400).json({ error: '이미지 파일이 없습니다.' });
     }
 
--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -1,18 +1,33 @@
 import multer from 'multer';
+import multerS3 from 'multer-s3';
+import { S3Client } from '@aws-sdk/client-s3';
 import path from 'path';
+import * as dotenv from 'dotenv';
 
-// Multer 업로드 설정
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/'); // 이미지 저장 경로
+dotenv.config(); // 최상단에 위치
+
+// AWS S3 설정
+const s3 = new S3Client({
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    cb(null, file.originalname.split('.')[0] + '-' + uniqueSuffix + path.extname(file.originalname)); // 실제 파일명 기반으로 설정
-  }
+  region: process.env.AWS_REGION,
 });
 
-export const upload = multer({ storage }).fields([
+// Multer S3 설정
+export const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.AWS_S3_BUCKET_NAME,
+    contentType: multerS3.AUTO_CONTENT_TYPE, // 자동으로 Content-Type 설정
+    key: (req, file, cb) => {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+      cb(null, file.originalname.split('.')[0] + '-' + uniqueSuffix + path.extname(file.originalname));
+    },
+  }),
+}).fields([
   { name: 'image', maxCount: 1 }, // image 필드 처리
-  { name: 'imageUrl', maxCount: 1 } // imageUrl 필드 처리
+  { name: 'imageUrl', maxCount: 1 }, // imageUrl 필드 처리
 ]);
+
